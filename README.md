950788ba18f08200ed7cf2aac230ecf1c141c964
hd-David <mwewahoby@gmail.com>
2024-02-21 01:23:20 -0800

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# 0x1B. C - Sorting algorithms & Big O
+
+This project delves into the world of sorting algorithms and their Big O time complexity analysis in C. You'll explore how to efficiently organize data using various techniques and understand the performance trade-offs involved.
---
+++ sort.h
@@ -0,0 +1,22 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/**
+ * print_array - Prints an array of integers
+ *
+ * @array: The array to be printed
+ * @size: Number of elements in @array
+ */
+void print_array(const int *array, size_t size);
+
+
+/**
+ * print_list - Prints a list of integers
+ *
+ * @list: The list to be printed
+ */
+void print_list(const listint_t *list);
+
+#endif /* SORT_H */
